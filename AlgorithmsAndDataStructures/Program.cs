739e81edd85bc88e82741b3389b754e72d694e6b
telman <babayevtolman@gmail.com>
2023-10-30 10:33:05 +0400

    Add project files.

---
+++ AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmsAndDataStructures.Sorting;
+
+namespace AlgorithmsAndDataStructures
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = Utils.FillArray(100);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Sorting/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures.Sorting
+{
+    internal class Utils
+    {
+        public static int[] FillArray(int num)
+        {
+            int[] res = new int[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                res[i] = num - i;
+            }
+
+            return res;
+        }
+    }
+}
